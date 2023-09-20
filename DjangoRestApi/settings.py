98c6cf786842714ca45b844f707405d1f85f6cc7
Dzmitry Tsahelnik <speshialforse@tut.by>
2023-09-20 22:12:08 +0300

    add req

--- DjangoRestApi/settings.py
+++ DjangoRestApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-83)mtt*odbvjycw(5z7gy9gw8zv=wbbjm7@3&4253-@8n8vjoy'
-
+# SECRET_KEY = 'django-insecure-83)mtt*odbvjycw(5z7gy9gw8zv=wbbjm7@3&4253-@8n8vjoy'
+SECRET_KEY = os.environ.get("SECRET_KEY");
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split("")
 
 
 # Application definition
