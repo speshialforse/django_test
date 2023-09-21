528c355ce162e04313b584bce3ba19351d87e767
Dzmitry Tsahelnik <speshialforse@tut.by>
2023-09-21 21:49:22 +0300

    js changes4

--- tutorials/views.py
+++ tutorials/views.py
@@ -10,7 +10,6 @@ from rest_framework.decorators import api_view
 
 from django.http import HttpResponse
 from django.template import loader
-import json
 
 @api_view(['GET', 'POST', 'DELETE'])
 def tutorial_list(request):
@@ -75,8 +74,9 @@ def tutorial_list_published(request):
 def members(request):
   mymembers = Tutorial.objects.all().values()
   template = loader.get_template('all_members.html')
+  jsonmymembers = TutorialSerializer(mymembers, many=True)
   context = {
     'mymembers': mymembers,
-    'jsonmymembers': json.dumps(mymembers),
+    'jsonmymembers': jsonmymembers,
   }
   return HttpResponse(template.render(context, request))
